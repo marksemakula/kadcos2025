242f765c6945e4e943207db766e2d5cc34209b79
Mark SEMAKULA™ <marksemakula@gmail.com>
2025-10-20 04:44:23 +0300

    u

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ export default {
       },
       fontFamily: {
         'urbanist': ['Urbanist', 'sans-serif'],
+        'marcellus': ['Marcellus', 'serif'], // Add this line
       },
     },
   },
